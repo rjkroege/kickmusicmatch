70f48300e5e1d8246876ebd48d4214a553162a6b
Robert Kroeger <rjkroege@liqui.org>
2023-07-11 08:19:36 -0600

    Lists all of the tracks

--- kickmusicmatch.js
+++ kickmusicmatch.js
@@ -3,5 +3,30 @@
 function run(argv) {
 	console.log("hello from main");
 	console.log(argv);
+
+
+	if (!Application('Music').running()) {
+		console.log("Music is not running, run Music first");
+		return "sad";
+		// TODO(rjk): rational exit status.
+	}
+
+	const musicapp = Application("Music");
+
+	// tracks is an array of track data. Properties are accessed with
+	// function notation.
+	const tracks = musicapp.tracks();
+
+	// Lists all of the tracks displaying aritst, album, trackname.
+	// This takes a while to spit out because I have many tracks and
+	// Edwood is not blindlingly fast.
+	tracks.forEach(x => {
+		console.log(x.artist(), x.album(), x.name());
+	});
+
+	
+
+	// console.log(JSON.stringify(playlists, null, 2)); // The null, 2 enables pretty-printing.
+
 }
 
